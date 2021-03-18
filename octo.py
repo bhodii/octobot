69194aa15e10bc98af82340f13496abdd69202eb
bhodi <bhodi@nowhere.com>
2021-03-17 22:42:01 -0400

    Added status and colors, removed av due to errors/rate limiting

--- octo.py
+++ octo.py
@@ -3,7 +3,6 @@ import os
 import types
 
 bot = discord.Client()
-#profile = discord.ClientUser()
 
 octo = types.SimpleNamespace()
 octo.happy = False
@@ -20,20 +19,32 @@ async def on_message(message):
   if message.content.startswith('OCTO FLIP'):
     await message.channel.send("OCTO FLIP!")
     octo.happy = not octo.happy
-  
+    role = discord.utils.get(message.guild.roles, name='Server Octopus')
+
+    if octo.happy:
+#      try:
+#        av = open('octo_happy.jpg','rb')
+#        await bot.user.edit(avatar=av.read(),color=0x3498db)
+#      finally:
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="everyone HAPPILY!"))
+        await role.edit(color=0x3498db)
+
+    else:
+#      try:
+#        av = open('octo_angry.jpg','rb')
+#        await bot.user.edit(avatar=av.read(),color=0x2ecc71)
+#      finally:
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="everyone ANGRILY!"))
+        await role.edit(color=0x2ecc71)
+
   if message.content.startswith('OCTO'):
     if octo.happy:
       picture = discord.File(open('octo_happy.jpg','rb'))
-      av = open('octo_happy.jpg','rb')
-      await bot.change_presence(status="OCTO HAPPY!")
       await message.channel.send(file=picture)
-      await bot.user.edit(avatar=av.read())
-
     else:
       picture = discord.File(open('octo_angry.jpg','rb'))
-      av = open('octo_angry.jpg','rb')
-      await bot.change_presence(status="OCTO ANGRY!")
       await message.channel.send(file=picture)
-      await bot.user.edit(avatar=av.read())
 
 bot.run(os.getenv('TOKEN'))
+
+# serverid 386127177114189824
