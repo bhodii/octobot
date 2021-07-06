f8248a7d76fae700b426ed2a990b9f9b251584a1
bhodi <bhodi@nowhere.com>
2021-07-06 11:37:34 -0400

    .

--- octo.py
+++ octo.py
@@ -7,6 +7,35 @@ bot = discord.Client()
 octo = types.SimpleNamespace()
 octo.happy = False
 
+def octo_flip:
+    await message.channel.send("OCTO FLIP!")
+    octo.happy = not octo.happy
+    role = discord.utils.get(message.guild.roles, name='Server Octopus')
+
+    if octo.happy:
+  #      try:
+  #        av = open('octo_happy.jpg','rb')
+  #        await bot.user.edit(avatar=av.read(),color=0x3498db)
+  #      finally:
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="everyone HAPPILY!"))
+        await role.edit(color=0x3498db)
+
+    else:
+  #      try:
+  #        av = open('octo_angry.jpg','rb')
+  #        await bot.user.edit(avatar=av.read(),color=0x2ecc71)
+  #      finally:
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name="everyone ANGRILY!"))
+        await role.edit(color=0x2ecc71)
+
+  if message.content.startswith('OCTO'):
+    if octo.happy:
+      picture = discord.File(open('octo_happy.jpg','rb'))
+      await message.channel.send(file=picture)
+    else:
+      picture = discord.File(open('octo_angry.jpg','rb'))
+      await message.channel.send(file=picture)
+
 @bot.event
 async def on_ready():
   print(f'We have logged in as {bot.user}')
